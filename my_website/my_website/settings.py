f060defb670d3d253cca537e72009a845cca1f4a
ScottLaans <221705@buas.nl>
2025-12-15 19:53:21 +0100

    fix

--- my_website/my_website/settings.py
+++ my_website/my_website/settings.py
@@ -140,7 +140,7 @@ import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
