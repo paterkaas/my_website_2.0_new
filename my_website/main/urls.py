1eff1e4613195edfea445fbf28ed5c013df1325f
ScottLaans <221705@buas.nl>
2025-12-15 21:55:20 +0100

    fix

--- my_website/main/urls.py
+++ my_website/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView  # <--- DEZE REGEL MISTE JE
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
-    path('privacy/', views.privacy, name='privacy'),                             
+    path('privacy/', views.privacy, name='privacy'),
     
     # De nieuwe pagina's
     path('allcamps/', views.allcamps, name='allcamps'),
@@ -13,9 +14,10 @@ urlpatterns = [
     path('depaal/', views.depaal, name='depaal'),
     path('terspegelt/', views.terspegelt, name='terspegelt'),
     path('efteling/', views.efteling, name='efteling'),
+
+    # Sitemap
     path('sitemap.xml', TemplateView.as_view(template_name='main/sitemap.xml', content_type='text/xml')),
 
-
-    # <--- HIER IS DE NIEUWE REGEL VOOR DE CHATBOT:
+    # Chatbot API
     path('api/chat', views.chat_api, name='chat_api'),
 ]
